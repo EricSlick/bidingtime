92aaf01a4f462badfe81eea5e29e41fe98e5eb4e
Eric Slick <ericpaulslick@gmail.com>
2012-09-15 22:13:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bidingtime::Application.config.secret_token = '98068789efa39a88611e5e7efa29706e4ab9d017310b40502acd780f1853a45f53452c87eb5bc73c4f0b4ba6683adf2ea5ce42f3f97dacf9c3a45ae259bee5e8'
